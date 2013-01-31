ead42a1af57358fef36de3056e36abd53e5caaf7
Jeff Weiss <weiss.jeffrey.m@gmail.com>
2013-01-31 15:37:24 -0500

    Changed reproductions.Rattay_1987.stimRatios so that is calculates thresholds with the sim_logger, rather than using the discrete data from reproduceRattay. Began generic_unmyelinated class. Hoc code should be complete, Matlab code hasn't been started yet.

---
+++ matlab_code/+NEURON/+cell/+axon/+generic_unmyelinated/@props/props.m
@@ -0,0 +1,24 @@
+classdef props < handle_light
+    %
+    %
+    %   Class: NEURON.cell.axon.generic_unmyelinated.props
+    %
+    % Relevant NEURON files:
+    % create_generic_axon.hoc - see method
+    % NEURON.cell.axon.generic.createCellInNeuron()
+    %
+    %   NEURON UNITS
+    %   =================================================
+    %   Section Units
+    %   L      - microns
+    %   diam   - microns
+    %   Ra     - ohm-cm 
+    
+   properties
+       
+       
+   end
+    
+    
+end
+    
