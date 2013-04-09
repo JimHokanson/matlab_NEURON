cc02eb73d5d61b996f91bbe463b8e250bca29769
Jim Hokanson <jim.hokanson@gmail.com>
2013-04-09 13:41:33 -0400

    Added Matt's regression equations for implementing Peterson model ...

--- matlab_code/+NEURON/+cell/+axon/+MRG/@props/props.m
+++ matlab_code/+NEURON/+cell/+axon/+MRG/@props/props.m
@@ -105,6 +105,11 @@ classdef props < handle_light
         %       NOTE: Values without defaults are diameter dependent
         %------------------------------------------------------------------
         
+       fiber_dependency_method = 1;
+       %1) Default
+       %2) Regression - Matt Schiefer
+       %3) 
+        
         %==================================================================
         %Morphological Parameters -----------------------------------------
         %==================================================================
@@ -248,6 +253,12 @@ classdef props < handle_light
             %NOTE: The dependent variables method will
             %set all necessary dirty bits
         end
+        function changeFiberDependencyMethod(obj,new_method)
+           
+           %TODO: move into function below
+           obj.fiber_dependency_method = new_method;
+           obj.populateDependentVariables(); 
+        end
         function changeProperty(obj,varargin)
             %
             %    INPUTS
@@ -299,42 +310,10 @@ classdef props < handle_light
             
             %See determineFiberEquations
             
-            fiber_diameter_all       = [5.7      7.3     8.7     10      11.5    12.8    14      15      16];
+            obj.changeFiberDiameterProps();
             
-            FIBER_INDEX = find(fiber_diameter_all == obj.fiber_diameter,1);
-            if isempty(FIBER_INDEX)
-                error('Unable to find specifications for given fiber size')
-            end
-            
-            %ROUGH DIAMETER OUTLINE
-            %--------------------------------------------------------------
-            %FIBER DIAMETER > AXON DIAMETER > NODE DIAMETER
-            %AXON DIAMETER = FLUT DIAMETER (PARANODE 2)
-            %NODE DIAMETER = MYSA DIAMETER (PARANODE 1)
-            
-            
-            internode_length_all     = [500      750     1000    1150    1250    1350    1400    1450    1500];
-            number_lemella_all       = [80       100     110     120     130     135     140     145     150];
-            %node_length             CONSTANT
-            node_diameter_all        = [1.9      2.4     2.8     3.3     3.7     4.2     4.7     5.0     5.5];
-            %paranode_length_1       CONSTANT
-            paranode_diameter_1_all  = [1.9      2.4     2.8     3.3     3.7     4.2     4.7     5.0     5.5];
-            %space_p1                CONSTANT
-            paranode_length_2_all    = [35       38      40      46      50      54      56      58      60];
-            paranode_diameter_2_all  = [3.4      4.6     5.8     6.9     8.1     9.2     10.4    11.5    12.7];
-            %space_p2                CONSTANT
-            %STIN LENGTH             DEPENDENT - delta_x_all,paranode_length_1,paranode_length_2_all,n_STIN
-            axon_diameter_all        = [3.4      4.6     5.8     6.9     8.1     9.2     10.4    11.5    12.7];
-            
-            obj.internode_length     = internode_length_all(FIBER_INDEX);
-            obj.number_lemella       = number_lemella_all(FIBER_INDEX);
-            obj.node_diameter        = node_diameter_all(FIBER_INDEX);
-            obj.paranode_diameter_1  = paranode_diameter_1_all(FIBER_INDEX);
-            obj.paranode_length_2    = paranode_length_2_all(FIBER_INDEX);
-            obj.paranode_diameter_2  = paranode_diameter_2_all(FIBER_INDEX);
-            obj.stin_seg_length      = (obj.internode_length - obj.node_length - 2*obj.paranode_length_1 - 2*obj.paranode_length_2)/obj.n_STIN;
-            obj.axon_diameter        = axon_diameter_all(FIBER_INDEX);
-            
+            obj.stin_seg_length      = (obj.internode_length - obj.node_length ...
+                - 2*obj.paranode_length_1 - 2*obj.paranode_length_2)/obj.n_STIN;
             
             %{
             
