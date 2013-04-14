e578361883717596689161891a2a0e22e59a7bf3
Jeff Weiss <weiss.jeffrey.m@gmail.com>
2013-04-14 19:28:57 -0400

    Fixed figure 6 (correct number of electrodes and electrode positions, correct fiber diameter). This actually looks really good for mdf1 thresholds! (not for mdf2)

--- matlab_code/+NEURON/+reproductions/@Peterson_2011/figure6.m
+++ matlab_code/+NEURON/+reproductions/@Peterson_2011/figure6.m
@@ -2,6 +2,7 @@ function figure6(obj)
 
 % figure 6
 % 100 us pulse, 10 um diameter, 200um EAS
+% arranged as in fig 1c, one stationary above 0,0,0, one moves further away
 
 % general props
 EAS = 200;
@@ -11,21 +12,20 @@ tissue_resistivity = [obj.resistivity_transverse obj.resistivity_transverse obj.
 % stimulus
 stim_start_time = 0.1;
 stim_duration = 0.1; % 100 us
-stim_amps = obj.eleven_electrode_amps; % defined in fig 1d
+stim_amps = {-1 -1};
 
 % initial electrode locations (actual depend on internodal, length, so this
 % is arbitrarily based on fig 8)
-elec_spacing = 650;
-electrode_locations = zeros(11,3);
+electrode_locations = zeros(2,3);
 electrode_locations(:,2) = EAS; % NOTE: cell will not need to be moved from origin
-unit_spacing = -5:5;
-electrode_locations(:,3) = unit_spacing*elec_spacing;
+% will vary electrode_locations(2,3) to move second electrode along axon
 
 % create sim
 xstim = NEURON.simulation.extracellular_stim.create_standard_sim(...
     'tissue_resistivity',tissue_resistivity,'electrode_locations',electrode_locations);
 cell = xstim.cell_obj;
 cell.props_obj.changeFiberDependencyMethod(2); % regression dependency
+cell.props_obj.changeFiberDiameter(fiber_diameter);
 electrodes = xstim.elec_objs;
 electrodes.setStimPattern(stim_start_time,stim_duration,stim_amps);
 
@@ -42,10 +42,9 @@ thresh_mdf1 = thresh_active;
 thresh_mdf2 = thresh_active;
 
 for i_elec_spacing = 1:N_elec_spacing
-    % move electrodes
-   elec_spacing = elec_spacing_all(i_elec_spacing);
-   electrode_locations(:,3) = unit_spacing*elec_spacing;
-   electrodes = moveElectrodesLocal(electrodes,electrode_locations);
+    % move second electrode
+   electrode_locations(2,3) = elec_spacing_all(i_elec_spacing);
+   electrodes(2).moveElectrode(electrode_locations(2,:));
    
    % active sim
    result = xstim.sim__determine_threshold(1);
@@ -70,11 +69,4 @@ ylabel('Threshold Error [%]','fontsize',fontsize)
 legend('Single Node Method','Weighted Sum Method')
 set(gca,'fontsize',fontsize - 2)
 
-end
-
-function elec_objs = moveElectrodesLocal(elec_objs,electrode_locations)
- N_electrodes = length(elec_objs);
- for i_elec = 1:N_electrodes
-    elec_objs(i_elec).moveElectrode(electrode_locations(i_elec,:));
- end
 end
