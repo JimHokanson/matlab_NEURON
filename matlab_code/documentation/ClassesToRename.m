8ced7afe22fea958b1d6c0eb03aee695c72f33b6
JimHokanson <jim.hokanson@gmail.com>
2013-04-21 14:09:37 -0400

    final commit of updates ...

---
+++ matlab_code/documentation/ClassesToRename.m
@@ -0,0 +1,8 @@
+%{
+
+%1) NEURON.comm_obj => comm
+%2) NEURON.comm_obj.java_comm_obj => java
+%3) 
+
+
+%}
---
+++ matlab_code/non_RNEL_functions/+arrayfcns/+xyz/getNumericDim.m
@@ -0,0 +1,17 @@
+function numeric_dim = getNumericDim(dim_input)
+%
+%
+%   numeric_dim = getNumericDim(dim_input)
+%
+%   FULL PATH:
+%   arrayfcns.xyz.getNumericDim
+
+if ischar(dim_input)
+    %TODO: Check char length
+    numeric_dim = strfind('xyz',dim_input);
+    if isempty(numeric_dim) || length(numeric_dim) > 1
+        error('Input should be x, y, or z')
+    end
+else
+    numeric_dim = dim_input;
+end
