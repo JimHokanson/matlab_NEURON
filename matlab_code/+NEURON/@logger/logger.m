19e83512122c4b0a70b2984989fd68f6e5a72539
Ivana Stevens <istevens@andrew.cmu.edu>
2013-06-18 18:08:31 -0400

    changed save/load to accomodate structs

--- matlab_code/+NEURON/@logger/logger.m
+++ matlab_code/+NEURON/@logger/logger.m
@@ -7,7 +7,7 @@ classdef logger < dynamicprops
     %   Known Helper Subclasses
     %   --------------------------------------------------------
     %   NEURON.logger.auto_logger
-    %   
+    %
     %
     %   Known implementations (for quick browsing)
     %   -------------------------------------------------------------------
@@ -21,13 +21,13 @@ classdef logger < dynamicprops
         a) Disect findMatch
     %}
     
-    %TODO: We might need to prefix these 
+    %TODO: We might need to prefix these
     properties (Abstract, Constant)
         LOGGER__VERSION     %Each logger should change the version if the data
         %they are logging changes. This will require creation of an update
         %method, called updateObj
         %
-        %   
+        %
         LOGGER__CLASS_NAME  %If a subclass of a class, then this should be the
         %fully resolved name of the super class. When not subclassed, then
         %this should just be the name of the class itself.
@@ -44,11 +44,12 @@ classdef logger < dynamicprops
     end
     
     properties
-%         log
+        %         log
         %.logger()
         LOGGER__n_trials = 0 %This should be internally maintained via add and remove
         %methods.
         LOGGER__parent %Reference to loggable parent
+        AUTO_PROPS
     end
     
     %Shared Methods         ===============================================
@@ -61,29 +62,29 @@ classdef logger < dynamicprops
             %
             %   Updates property: LOGGER__n_trials
             %
-
+            
             next_match_id        = obj.LOGGER__n_trials + 1;
             obj.LOGGER__n_trials = next_match_id;
-        end 
+        end
     end
     methods
         function obj = logger(parent)
-           obj.LOGGER__parent = parent; 
+            obj.LOGGER__parent = parent;
         end
         
         function editParent(obj,parent)
-           obj.LOGGER__parent = parent; 
+            obj.LOGGER__parent = parent;
         end
         
         %Adding a new entry outline:
         %--------------------------------------------------------
-
+        
         %3) obj.saveHelper();
         %
         %   This next step is optional but typical ...
         %
         %4) ID = obj.getID(next_match_id);
-        % 
+        %
         %NOTE: It would be nice to wrap this into a function that takes a
         %save handle and evaluates it ...
         %TODO: Implement this ...
@@ -94,29 +95,29 @@ classdef logger < dynamicprops
             ID = obj.getID(next_match_id);
         end
         function ID = getID(obj,match_index)
-           %getID
-           %
-           %    ID = getID(obj,*match_index)
-           %
-           %    ID = getID(obj)
-           %
-           %    INPUTS
-           %    ===========================================================
-           %    match_index : (default []), typically the row of the
-           %                matching entry, if no match is found an empty
-           %                represents a null id
-           %
-           %    FULL PATH:
-           %    NEURON.logger.getID
-           
-           if ~exist('match_index','var')
-              match_index = [];
-           end
-
-           ID = NEURON.logger.ID(obj.LOGGER__CLASS_NAME, obj.LOGGER__TYPE, match_index);
+            %getID
+            %
+            %    ID = getID(obj,*match_index)
+            %
+            %    ID = getID(obj)
+            %
+            %    INPUTS
+            %    ===========================================================
+            %    match_index : (default []), typically the row of the
+            %                matching entry, if no match is found an empty
+            %                represents a null id
+            %
+            %    FULL PATH:
+            %    NEURON.logger.getID
+            
+            if ~exist('match_index','var')
+                match_index = [];
+            end
+            
+            ID = NEURON.logger.ID(obj.LOGGER__CLASS_NAME, obj.LOGGER__TYPE, match_index);
         end
     end
-
+    
     %Saving/Loading/Pathing ===============================================
     methods
         %NOTE: Instead of being helpers, they could be the main
@@ -125,55 +126,75 @@ classdef logger < dynamicprops
         %
         %For example, when saving the auto_logger, we don't need to save
         %the AUTO_INFO property
-        function saveHelper(obj,properties_remove)
-            %
-            %
-            %   This function converts the object to a structure and saves it.
-            %   By saving the object as a structure we ensure that we can load
-            %   it on any computer, regardless of whether the class definition
-            %   code exists or not.
-            
-            
-            w = warning('off','MATLAB:structOnObject');
-            s = struct(obj);
-            warning(w);
-            
-            %Very important line ...
-            if isfield(s,'LOGGER__parent')
-                s = rmfield(s,'LOGGER__parent');
-            end
-            
-            if exist('properties_remove','var')
-                s = rmfield(s,properties_remove);%#ok<NASGU>
-            end
-            
+        %         function saveHelper(obj,properties_remove)
+        %             %
+        %             %
+        %             %   This function converts the object to a structure and saves it.
+        %             %   By saving the object as a structure we ensure that we can load
+        %             %   it on any computer, regardless of whether the class definition
+        %             %   code exists or not.
+        %
+        %             w = warning('off','MATLAB:structOnObject');
+        %             s = struct(obj);
+        %             warning(w);
+        %
+        %             %Very important line ...
+        %             if isfield(s,'LOGGER__parent')
+        %                 s = rmfield(s,'LOGGER__parent');
+        %             end
+        %
+        %             if exist('properties_remove','var')
+        %                 s = rmfield(s,properties_remove);%#ok<NASGU>
+        %             end
+        %
+        %             save_path = obj.getSaveDataPath;
+        %             save(save_path,'s');
+        %         end
+        %         function loadHelper(obj)
+        %             %
+        %             %
+        %             %   loadHelper(obj)
+        %             %
+        %             %   NOTE: We might be able to update the toObject code
+        %             %   to not try and assign constant properties ...
+        %
+        %             save_path = obj.getSaveDataPath;
+        %             if exist(save_path,'file')
+        %                 h = load(save_path);
+        %                 s = h.s;
+        %
+        %                 if obj.LOGGER__VERSION ~= s.LOGGER__VERSION
+        %                     s = obj.update(s);
+        %                 end
+        %
+        %                 result = sl.struct.toObject(obj,s); %#ok<NASGU>
+        %                 %NOTE: result will eventually be a class that dictactes
+        %                 %what happens, since the input class is a handle, the props
+        %                 %are assigned in the function, and we don't need to do
+        %                 %anything with the output class
+        %                 %
+        %                 %result.raiseError - not yet implented ...
+        %             end
+        %         end
+        function saveLog(obj)
+            s = struct; 
+            s.AUTO_PROPS        = obj.AUTO_PROPS;
+            s.LOGGER__VERSION   = obj.LOGGER__VERSION;
+            s.LOGGER__n_trials  = obj.LOGGER__n_trials; %#ok
             save_path = obj.getSaveDataPath;
             save(save_path,'s');
         end
-        function loadHelper(obj)
-            %
-            %   
-            %   loadHelper(obj)
-            %
-            %   NOTE: We might be able to update the toObject code
-            %   to not try and assign constant properties ...
-            
+        function loadLog(obj)
             save_path = obj.getSaveDataPath;
             if exist(save_path,'file')
                 h = load(save_path);
                 s = h.s;
-                
+
                 if obj.LOGGER__VERSION ~= s.LOGGER__VERSION
                     s = obj.update(s);
                 end
-                
-                result = sl.struct.toObject(obj,s); %#ok<NASGU>
-                %NOTE: result will eventually be a class that dictactes
-                %what happens, since the input class is a handle, the props
-                %are assigned in the function, and we don't need to do
-                %anything with the output class
-                %
-                %result.raiseError - not yet implented ...
+                obj.AUTO_PROPS       = s.AUTO_PROPS;
+                obj.LOGGER__n_trials = s.LOGGER__n_trials;
             end
         end
         function save_base_path = getClassPath(obj)
@@ -204,7 +225,7 @@ classdef logger < dynamicprops
         %   This method needs to be implemented if a subclass changes its
         %   version number
         function new_data_struct_handle = update(~,~) %#ok<STOUT>
-           error('update() implementation needed in subclass') 
+            error('update() implementation needed in subclass')
         end
     end
     
