269db08edff1de8555f4c45d9ffa623a4ba7147f
Ivana Stevens <istevens@andrew.cmu.edu>
2013-06-20 15:00:14 -0400

    made persistant

--- matlab_code/+NEURON/+simulation/+extracellular_stim/@xstim_logger/xstim_logger.m
+++ matlab_code/+NEURON/+simulation/+extracellular_stim/@xstim_logger/xstim_logger.m
@@ -12,20 +12,34 @@ classdef xstim_logger < NEURON.logger.ID_logger
     
     
     properties (Constant)
-       ID_LOGGER__PROPS = {'elec_objs' 'cell_obj' 'props' 'tissue_obj'};
+        %ID_LOGGER__PROPS = {'elec_objs' 'cell_obj' 'props' 'tissue_obj'};
+        ID_LOGGER__PROPS = {'elec_objs' 'props' 'tissue_obj'};
     end
     
-    methods
+    methods(Access = private)
         function obj = xstim_logger(varargin)
-           obj@NEURON.logger.ID_logger(varargin{:}); 
+            obj@NEURON.logger.ID_logger(varargin{:});
+        end
+        
+        %notes:
+        % MIMs should prolly be a singeton, but this does not necesarily have
+        % to be.. this is because it will simply be handling abunch of loggers
+        % that are already singletons
+        %
+        % This also does not necessarily need to be able to find itself does
+        % it? Or is MIMs just the struct that we will save... methods(Static)
+    end
+    methods(Static)
+        function obj = getLogger(varargin)
+            persistent x_logger
+            if isempty(x_logger)
+                obj = NEURON.simulation.extracellular_stim.xstim_logger(varargin{:});
+                x_logger = obj;
+            else
+                x_logger.editParent(varargin{:});
+            end
+            obj = x_logger;
         end
     end
-    %notes:
-    % MIMs should prolly be a singeton, but this does not necesarily have
-    % to be.. this is because it will simply be handling abunch of loggers
-    % that are already singletons
-    %
-    % This also does not necessarily need to be able to find itself does
-    % it? Or is MIMs just the struct that we will save...
 end
 
