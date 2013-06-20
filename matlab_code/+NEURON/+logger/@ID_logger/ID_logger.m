72d1831c010f50ea663451ab78cd69464f959c7e
Ivana Stevens <istevens@andrew.cmu.edu>
2013-06-20 14:59:10 -0400

    added save and load code

--- matlab_code/+NEURON/+logger/@ID_logger/ID_logger.m
+++ matlab_code/+NEURON/+logger/@ID_logger/ID_logger.m
@@ -1,6 +1,6 @@
 classdef ID_logger < NEURON.logger
     %
-    %   
+    %
     %   Class:
     %   NEURON.logger.ID_logger
     %
@@ -15,33 +15,47 @@ classdef ID_logger < NEURON.logger
     %   NEURON.logger.multi_id_manager
     
     properties (Abstract,Constant)
-       ID_LOGGER__PROPS %Currently just a cell array of a list of properties
-       %that are themselves classes (LOGGABLE classes)
+        ID_LOGGER__PROPS %Currently just a cell array of a list of properties
+        %that are themselves classes (LOGGABLE classes)
     end
     
     properties
-       ID_LOGGER__multi_id_manager %Class: NEURON.logger.multi_id_manager
+        ID_LOGGER__multi_id_manager %Class: NEURON.logger.multi_id_manager
     end
     
     %CONSTRUCTOR   %=======================================================
     methods
         function obj = ID_logger(parent)
-           obj@NEURON.logger(parent);
-           
-           save_base_path = obj.getClassPath;
-           
-           obj.ID_LOGGER__multi_id_manager = ...
-                    NEURON.logger.multi_id_manager(save_base_path);
+            obj@NEURON.logger(parent);
+            
+            save_base_path = obj.getClassPath;
+            
+            obj.ID_LOGGER__multi_id_manager = ...
+                NEURON.logger.multi_id_manager(save_base_path);
         end
     end
     
     %SAVE AND LOAD  %======================================================
     methods
         function saveObject(obj)
-           obj.ID_LOGGER__multi_id_manager.saveObject();
+            loggableClasses = obj.getLoggableClasses();
+            n_props       = length(loggableClasses);
+            for iObj = 1:n_props
+                temp = loggableClasses{iObj};
+                log = temp.getLogger();
+                log.saveLog();
+            end
+            obj.ID_LOGGER__multi_id_manager.saveObject();
         end
         
         function loadObject(obj)
+            loggableClasses = obj.getLoggableClasses();
+            n_props       = length(loggableClasses);
+            for iObj = 1:n_props
+                temp = loggableClasses{iObj};
+                log = temp.getLogger();
+                log.loadLog();
+            end
             obj.ID_LOGGER__multi_id_manager.loadObject();
         end
     end
@@ -49,57 +63,54 @@ classdef ID_logger < NEURON.logger
     %PROCESSING   %========================================================
     methods
         function ID = find(obj,varargin)
-           %
-           %
-           %    STATUS: Done, but untested
-           
-           in.create_if_not_found = true;
-           in = sl.in.processVarargin(in,varargin);
-           
-           loggable_classes_cell_array = obj.getLoggableClasses;
-           
-           %Call method of ID manager
-           [matching_row,is_new] = obj.ID_LOGGER__multi_id_manager.find(...
-                        loggable_classes_cell_array,...
-                        'create_if_not_found',in.create_if_not_found);
-           
-           found = ~isempty(matching_row);
-           
-           if ~found()
-               ID = obj.getID([]); 
-           else
-               if is_new
-                  %??? - do I need to make this anonymous ????
-                  %(@(x)saveObject(obj))
-                  ID = obj.updateIDandSave(@obj.saveObject);
-               else
-                  %NEURON.logger.getID
-                  ID = obj.getID(matching_row);
-               end
-           end
-           
+            %
+            %
+            %    STATUS: Done, may need more testing
+            
+            in.create_if_not_found = true;
+            in = sl.in.processVarargin(in,varargin);
+            
+            loggable_classes_cell_array = obj.getLoggableClasses;
+            
+            %Call method of ID manager
+            [matching_row,is_new] = obj.ID_LOGGER__multi_id_manager.find(...
+                loggable_classes_cell_array,...
+                'create_if_not_found',in.create_if_not_found);
+            
+            found = ~isempty(matching_row);
+                        
+            if ~found()
+                if (is_new && in.create_if_not_found)
+                    ID = obj.updateIDandSave(@obj.saveObject);
+                else
+                    ID = obj.getID([]);
+                end
+            else
+                ID = obj.getID(matching_row);
+            end
+            
         end
         function obj_cell_array = getLoggableClasses(obj)
-           %
-           %    obj_cell_array = getLoggableClasses(obj)
-           %
-           %    
-           %    STATUS: Done, but untested
-           
+            %
+            %    obj_cell_array = getLoggableClasses(obj)
+            %
+            %
+            %    STATUS: Done, may need more testing
+            
             p = obj.LOGGER__parent;
             
             props_local   = obj.ID_LOGGER__PROPS;
             n_props       = length(props_local);
             
             obj_cell_array = cell(1,n_props);
-            for iObj = 1:n_props
-               
-               cur_prop = props_local{iProp};
-               %NOTE: cur_prop is a loggable class
-               
-               obj_cell_array{iProp} = p.getProps(cur_prop); %is this ok? 
-               %We might want to later declare that all classes like xstim
-               %have a getProps class... anyway these props are private :P
+            for iProp = 1:n_props
+                
+                cur_prop = props_local{iProp};
+                %NOTE: cur_prop is a loggable class
+                
+                obj_cell_array{iProp} = p.getProps(cur_prop); %is this ok?
+                %We might want to later declare that all classes like xstim
+                %have a getProps class... anyway these props are private :P
             end
             
         end
