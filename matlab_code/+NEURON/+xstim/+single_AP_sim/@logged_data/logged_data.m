b6ce12ad3173b6361e0abe3e4e90413bfab368fe
Ivana Stevens <istevens@andrew.cmu.edu>
2013-06-28 16:30:03 -0400

    added code for request_handler/logged_data

--- matlab_code/+NEURON/+xstim/+single_AP_sim/@logged_data/logged_data.m
+++ matlab_code/+NEURON/+xstim/+single_AP_sim/@logged_data/logged_data.m
@@ -1,44 +1,101 @@
-classdef logged_data
-% This class is in charge of saveing/loading each data instance and
+classdef logged_data < handle
+% This class is in charge of saving/loading each data instance and
 % maintaining which predictor is being used to generate this information
 % especially useful for testing, we are going to want to insure that the
 % different predictor methods do not generate different outcomes.
     
     properties
-        cell_locations
         pos_threshold
         neg_threshold
         sign
-    end
-    
+    end    
     properties
         predictor
+        parent
+    end
+    properties
+        stim_base
+        stim_old
+        thresholds_old
+        cell_locations_old
+    end
+    properties(Constant)
+        VERSION = 1;
     end
     
     methods
-        function obj = logged_data(cell_locations, sign)
+        function obj = logged_data(xstim, sign)
             % populates properties 
+            obj.parent = xstim;
+            % log = xstim.getLogger();
+            obj.sign = sign;
         end
-        function [sign, cell_locations] = load_CellLocations(obj)
+        function load_data(obj)
             %I'm not exactly sure what form the output should take, but
             %this would return the cell_locations of previously run trials
             %as well as the sign they were run with
-            % opens from path and reformats 
+            % opens from path and reformats
+            h = load(savePath);
+            if (h.VERSION ~= obj.VERSION)
+                %throw an error
+            end
+            obj.thresholds_old     = h.thresholds_old;
+            obj.stim_base          = h.stim_base;
+            %obj.recreateStimuli();
+            obj.cell_locations_old = h.cell_locations_old;
         end
-        function save_CellLocations()
+        function save_data(obj)
             %called by request_handler thru predictor_obj?
             % saves at right path
+            savePath = get_savePath();
+            h = struct;
+            h.VERSION            = obj.VERSION;
+            h.stim_base          = obj.stim_base;
+            h.thresholds_old     = obj.thresholds_old;
+            h.cell_locations_old = obj.cell_locations_old;
+            save(savePath, h);
         end
-        function get_savePath()
+        function get_savePath(obj)
             % may or may not actually be necessary here...
         end
-        function stim = getStimulus(cell_locations, sign)
-            % option A: sends command to get stored stimulus vector. 
-            % option B: sends command to obtain old cell_locations and then
-            %   regenerates their respective stimuli (as well as the new)
-            % This method would need to send its data to the predictor
-            % model.
+        function recreateStimuli(obj)
+            % should be called from predictor
+             linear_elec = obj.stim_base;
+             stim = [];
+%             %==================================================
+%             % part of how we might generate the stim_old
+%             %==================================================
+%             index = 1;
+%             len = length(linear_elec);
+%             while index > len
+%                 n_elec = linear_elec(index);
+%                 elec_top = index + 1;
+%                 elec_end = index + 3*n_elec;
+%                 temp = linear_elec(elec_top:elec_end);
+%                 xyz_all = reshape(temp,3,n_elec)';
+%                 stim_temp = mergeStimTimes(xyz_all); 
+%                 % that code ^ will need alterations...and getLogData???
+%                 stim = [stim stim_temp];
+%                 index = elec_end + 1;
+%             end
+            obj.stim_old = stim; 
         end
+        
+        %{
+        %          Not Sure Yet Where This Will Go
+        %===================================================
+        %    part of how we might generate the stim_base
+        %===================================================
+        n_electrodes = length(electrodes);
+        xyz_all = [];
+        
+        for iElec = 1:n_electrodes
+            xyz_all = [xyz_all electrodes(iElec).xyz];
+        end
+        elec_data = [n_electrodes xyz_all];
+        obj.stim_base = [obj.stim_base elec_data];      
+        %} 
+        
     end
 
 end
