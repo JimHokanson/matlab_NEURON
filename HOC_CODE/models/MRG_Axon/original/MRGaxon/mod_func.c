55c864fb28f2727c5246fde61505c55ef253eb0c
Jim Hokanson <jim.hokanson@gmail.com>
2019-09-05 09:15:32 -0400

    recompiled mrg mod files

--- HOC_CODE/models/MRG_Axon/original/MRGaxon/mod_func.c
+++ HOC_CODE/models/MRG_Axon/original/MRGaxon/mod_func.c
@@ -8,8 +8,6 @@ modl_reg(){
     if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
 	fprintf(stderr, "Additional mechanisms from files\n");
 
-fprintf(stderr," AXNODE.mod");
 fprintf(stderr, "\n");
     }
-_AXNODE_reg();
 }
